f38862c25dd1f5f8c512227c3e390b582046a072
ssanti86 <monsterspeed02@gmail.com>
2023-12-06 22:21:56 -0300

    rebase

--- router.php
+++ router.php
@@ -5,7 +5,7 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-require_once '../config.php';
+require_once './config.php';
 require_once './libs/router.php';
 
 require_once './controllers/dashboard.controller.php';
